ee94468abea19a0bb870319e07192eff78e35a38
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-17 23:31:24 -0500

    Add files via upload

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(asc)
+    asc.sort
+end
 
+def sort_array_desc(desc)
+    desc.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(char_count)
+    char_count.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(swap)
+    swap[1], swap[2] = swap[2], swap[1]
+    swap
+end
+
+def reverse_array(rev)
+    rev.reverse
+end
+
+def kesha_maker(kesha)
+    kesha.each {|name| name[2] = "$"}
+end
+
+def find_a(find)
+    find.select {|a| a.start_with?("a")}
+end
+
+def sum_array(add)
+    add.inject(0){|sum,x| sum + x }
+end
+
+def add_s(letter_s)
+    letter_s.each_with_index.collect do |word, int|
+        if int == 1
+            word
+        else
+            word + "s"
+        end
+    end
+end
